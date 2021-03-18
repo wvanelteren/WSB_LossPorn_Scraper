c15cf642001310441ae0f77983ac02e1b1499009
Wouter van Elteren <wvanelteren@gmail.com>
2021-03-18 14:58:23 +0100

    added comments and excel save feature

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import praw
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import xlsxwriter
 
 reddit = praw.Reddit(
     client_id='bUAYwo47haEZlg',
@@ -10,7 +11,7 @@ reddit = praw.Reddit(
 )
 
 SUBREDDIT = 'wallstreetbets'
-LOSS_POSTS = reddit.subreddit(SUBREDDIT).search('flair:"loss"', sort='new', time_filter='month', limit=250)
+LOSS_POSTS = reddit.subreddit(SUBREDDIT).search('flair:"loss"', sort='new', time_filter='month', limit=5)
 
 
 # Determines upvote threshold by identifying outliers below the lower whisker of a boxplot
@@ -28,10 +29,10 @@ def get_upvote_threshold(posts):
     # Determine Interquartile Range
     q1 = np.quantile(posts_upvote_ratio, 0.25)
     q3 = np.quantile(posts_upvote_ratio, 0.75)
-    iqr = q3-q1
+    iqr = q3 - q1
 
     # Formula for determining lower whisker
-    lower_whisker = q1-1.5*iqr
+    lower_whisker = q1 - 1.5 * iqr
 
     print(f"Upvote threshold for submissions is {lower_whisker}")
     return lower_whisker
@@ -62,15 +63,41 @@ def get_posts():
     return posts
 
 
+def get_top_level_comments(url):
+    post = reddit.submission(url=url)
+    post.comments.replace_more(limit=0)
+
+    comments = []
+
+    for top_level_comment in post.comments:
+        comments.append([
+            top_level_comment.author,
+            top_level_comment.body,
+            top_level_comment.score,
+            top_level_comment.is_submitter,
+            'https://www.reddit.com' + top_level_comment.permalink
+        ])
+    return comments
+
+
 def main():
+    writer = pd.ExcelWriter('wsb_loss-porn.xlsx', engine='xlsxwriter')
+
+    # Write Posts Dataframe to sheet one
     posts = get_posts()
-    posts = pd.DataFrame(posts, columns=['title', 'score', 'upvote ratio', 'num_comments', 'body', 'url'])
-    print(posts)
+    posts_pd = pd.DataFrame(posts, columns=['title', 'score', 'upvote ratio', 'num_comments', 'body', 'url'])
+    posts_pd.to_excel(writer, sheet_name='Sheet1')
+
+    # Write top level comments of each post to a different worksheet
+    for count, post in enumerate(posts):
+        comments = get_top_level_comments(post[5])
+        comments_pd = pd.DataFrame(comments, columns=['author', 'body', 'score', 'is_submitter?', 'url'])
+        # Sheet 0 does not exist and sheet 1 is reserved for the posts
+        comments_pd.to_excel(writer, sheet_name='Sheet' + str(2 + count))
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.save()
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
